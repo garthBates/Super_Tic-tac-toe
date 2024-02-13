05f03810e99c9e01212ef8540a1a7f22039382ab
Garth <garthbates@protonmail.com>
2024-02-12 17:09:34 -0800

    Filled window with beige background

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pygame
 
 WIDTH, HEIGHT = 700, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-
+pygame.display.set_caption("Super TIC-TAC-TOE")
+WHITE = (255,255,255)
+BEIGE = (245,245,220)
 
 def main():
 
@@ -11,6 +13,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        WIN.fill(BEIGE)
+        pygame.display.update()
 
     pygame.quit()
 
