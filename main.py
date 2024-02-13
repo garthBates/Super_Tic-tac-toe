1b6d67bbe895a68dd7779548a11b5f6ab64f4517
Garth <garthbates@protonmail.com>
2024-02-12 17:02:56 -0800

    Set up event loop

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ WIDTH, HEIGHT = 500, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def main():
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
