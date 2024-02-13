f314ee45e84fce1fbf4a01236edab45fdd5061ea
Garth <garthbates@protonmail.com>
2024-02-12 17:13:11 -0800

    Set framerate to 60 FPS

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ pygame.display.set_caption("Super TIC-TAC-TOE")
 WHITE = (255,255,255)
 BEIGE = (245,245,220)
 
+FPS = 60
+
 def draw_window():
     WIN.fill(BEIGE)
     pygame.display.update()
 
 def main():
-
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
